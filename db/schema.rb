a9963fe7c9cf76dab0cf29f9bf5a338d5d37a452
vlandham <vlandham@vUbuntu.(none)>
2010-05-24 10:27:42 -0500

    added user scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100521154339) do
+ActiveRecord::Schema.define(:version => 20100524152344) do
 
   create_table "maps", :force => true do |t|
     t.string   "name"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
