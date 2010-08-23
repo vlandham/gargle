0ebe3f634133e6a682ebf68a7758253300e28729
Jim Vallandingham <jim.vallandingham@garmin.com>
2010-08-23 10:53:45 -0500

    added name to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
+  
+  def name
+    self.email.split(".")[0]
+  end
 end
