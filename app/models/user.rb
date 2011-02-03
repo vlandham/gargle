6ad48803fb380c6e12af248c732ee275052fc900
Jim Vallandingham <jim.vallandingham@garmin.com>
2011-02-03 14:02:17 -0600

    added devise views to project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ActiveRecord::Base
   has_many :maps, :dependent => :destroy
-  
+
   # Include default devise modules. Others available are:
   # :http_authenticatable, :token_authenticatable, :confirmable, :lockable, :timeoutable and :activatable
-  devise :registerable, :authenticatable, :recoverable,
+  devise :registerable, :database_authenticatable, :recoverable,
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
-  
+
   def name
     self.email.split(".")[0]
   end
 end
+
