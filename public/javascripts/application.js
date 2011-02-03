4d6340a9bb97a0638562fe3361d5a87dd9f459a0
Jim Vallandingham <jim.vallandingham@garmin.com>
2011-02-03 14:14:39 -0600

    display alerts and hide them via javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,30 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function showMarker(marker, content)
+// Show a marker and open it's info window
+{
+	Effect.ScrollTo('map');
+    google_map.closeInfoWindow(); // else get problems
+    eval(marker + ".openInfoWindowHtml(\'"+content+"\');");
+    //eval("GEvent.trigger(" + marker[0] + ", click)");
+}
+
+function hide_notice()
+{
+	if ($('flash_notice') != null) {
+		Effect.Shake('flash_notice');
+		Effect.Fade.delay(2, 'flash_notice');
+	}
+	if ($('flash_alert') != null) {
+		Effect.Shake('flash_alert');
+		Effect.Fade.delay(2, 'flash_alert');
+	}
+	if ($('flash_warning') != null) {
+		Effect.Shake('flash_warning');
+		Effect.Fade.delay(2, 'flash_warning');
+	}
+}
+
+window.onload=hide_notice;
+
