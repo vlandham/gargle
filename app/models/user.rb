04addc79550263d6906c0fc4e1895896f07a6bfc
Jim Vallandingham <jim.vallandingham@garmin.com>
2011-02-04 10:43:47 -0600

    name now breaks on at sign if there is no period

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :http_authenticatable, :token_authenticatable, :confirmable, :lockable, :timeoutable and :activatable
-  devise :registerable, :database_authenticatable, :recoverable,
+  devise :registerable, :database_authenticatable, 
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
 
   def name
-    self.email.split(".")[0]
+    self.email.split("@")[0].split(".")[0]
   end
 end
 
